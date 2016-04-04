3d2114296b6b6f41cf545fea98dca424ff69f3a5
Cameron Moy <camoy@umd.edu>
2016-04-04 14:26:02 -0400

    Add support for Lua

---
+++ lib/travis/build/script/lua.rb
@@ -0,0 +1,81 @@
+module Travis
+  module Build
+    class Script
+      class Lua < Script
+        DEFAULTS = {
+          lua: '5.3'
+        }
+
+        def export
+          super
+
+          sh.export 'TRAVIS_LUA_VERSION', version, echo: false
+        end
+
+        def setup
+          super
+
+          sh.echo 'Lua for Travis-CI is not officially supported, ' \
+                  'but is community maintained.', ansi: :green
+          sh.echo 'Please file any issues using the following link',
+                  ansi: :green
+          sh.echo '  https://github.com/travis-ci/travis-ci/issues' \
+                  '/new?labels=community:lua', ansi: :green
+          sh.echo 'and mention \`@camoy\`, \`@mpeterv\`, and \`@\`'\
+                  ' in the issue', ansi: :green
+
+          unless lua_version_valid?
+            sh.failure "\"#{version}\" is an invalid version of Lua.\nView valid versions of Lua at https://docs.travis-ci.com/user/languages/lua/"
+          end
+
+          sh.fold('lua-install') do
+            if lua_version_valid?
+              sh.echo "Installing Lua", ansi: :yellow
+              sh.cmd 'pip install hererocks'
+              sh.cmd "hererocks here -r^ --#{lua_hererocks_version}"
+              sh.export 'PATH', '$PATH:$PWD/here/bin'
+              sh.cmd 'luarocks install busted'
+            end
+          end
+        end
+
+        def announce
+          super
+
+          sh.cmd "lua -v"
+          sh.echo ''
+        end
+
+        def install
+          sh.if '-f *.rockspec'do
+            sh.cmd 'luarocks make'
+          end
+        end
+
+        def script
+          sh.cmd 'busted'
+        end
+
+        private
+
+        def version
+          config[:lua].to_s.shellescape
+        end
+
+        def lua_version_valid?
+          not lua_hererocks_version.nil?
+        end
+
+        def lua_hererocks_version
+          case version
+          when '5.1' then 'lua=5.1'
+          when '5.2' then 'lua=5.2'
+          when '5.3' then 'lua=5.3'
+          when 'luajit-2.0' then 'luajit=2.0'
+          when 'luajit-2.1' then 'luajit=2.1'
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/build/script/lua_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+describe Travis::Build::Script::Lua, :sexp do
+  let(:data)   { payload_for(:push, :lua) }
+  let(:script) { described_class.new(data) }
+  subject      { script.sexp }
+
+  it_behaves_like 'a build script sexp'
+
+  it "announces `lua -v`" do
+    should include_sexp [:cmd, "lua -v", echo: true]
+  end
+
+  it "runs tests by default" do
+    should include_sexp [:cmd, "busted", echo: true, timing: true]
+  end
+
+  context "versions" do
+    it "installs Lua 5.3 version by default" do
+      should include_sexp [:cmd, "hererocks here -r^ --lua=5.3",
+        assert: true, echo: true, timing: true]
+    end
+
+    it "installs Lua 5.2 version when specified" do
+      data[:config][:lua] = "5.2"
+      should include_sexp [:cmd, "hererocks here -r^ --lua=5.2",
+        assert: true, echo: true, timing: true]
+    end
+
+    it "installs Lua 5.1 version when specified" do
+      data[:config][:lua] = "5.1"
+      should include_sexp [:cmd, "hererocks here -r^ --lua=5.1",
+        assert: true, echo: true, timing: true]
+    end
+
+    it "installs LuaJIT 2.0 version when specified" do
+      data[:config][:lua] = "luajit-2.0"
+      should include_sexp [:cmd, "hererocks here -r^ --luajit=2.0",
+        assert: true, echo: true, timing: true]
+    end
+
+    it "installs LuaJIT 2.1 version when specified" do
+      data[:config][:lua] = "luajit-2.1"
+      should include_sexp [:cmd, "hererocks here -r^ --luajit=2.1",
+        assert: true, echo: true, timing: true]
+    end
+
+    it 'throws a error with a invalid version' do
+      data[:config][:lua] = "foo"
+      should include_sexp [:echo, "\"foo\" is an invalid version of Lua.\nView valid versions of Lua at https://docs.travis-ci.com/user/languages/lua/"]
+    end
+  end
+end
